ddaf495d310ae5370216b123160c3b1cc687844c
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-12-24 20:32:29 +0530

    server and handlers defined.

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	app := fiber.New(fiber.Config{
+		Prefork:       true,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Fiber",
+		AppName:       "share_book v0.0.1",
+	})
+
+	log.Fatal(app.Listen(":8000"))
+}
