3a42480eefa2b994a1294a87609a4b12a779e8a1
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-12-25 00:37:42 +0530

    adds put request

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/2k4sm/share_book/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -15,6 +16,18 @@ func main() {
 		ServerHeader:  "Fiber",
 		AppName:       "share_book v0.0.1",
 	})
+
+	// Adds a new book for share.
+	app.Put("/api/v1/booky", handlers.ShareBook)
+
+	// Retrieves all the books in share.
+	// app.Get()
+
+	// // Borrows a book from the shared books for a specified time.
+	// app.Put()
+
+	// // Returns the shared book.
+	// app.Post()
 
 	log.Fatal(app.Listen(":8000"))
 }
